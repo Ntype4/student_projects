2f57e47eb7cc11982cdce823c77ba8d103e50eab
Ntype4 <student@student.machine>
2025-11-21 14:09:51 +0000

    studentmachine send

---
+++ cpp/c/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+	ifstream pliki("wynik.txt");
+	if(!plik.is_open()){
+	cout << "nie udalo sie otworzyc\n";
+	return 1;
+}
+	string licz;
+	int suma;
+	int liczba;
+
+	while(getline(plik, licz)){
+	cout << licz << endl;
+}
+
+
+
+
+
+
+	plik.close();
+	return 0;
+}
